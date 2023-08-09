18c26a91ec7bae7e6a252db87ee633455be8b836
Manzz <moonlightmadness131@gmail.com>
2023-08-09 08:45:52 +0000

    Trial 2

--- src/main/java/com/team/ram/backend/entities/Stock.java
+++ src/main/java/com/team/ram/backend/entities/Stock.java
@@ -17,5 +17,57 @@ public class Stock implements Serializable {
     private String buyOrSell;
     private int statusCode;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStockTicker() {
+        return stockTicker;
+    }
+
+    public void setStockTicker(String stockTicker) {
+        this.stockTicker = stockTicker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getBuyOrSell() {
+        return buyOrSell;
+    }
+
+    public void setBuyOrSell(String buyOrSell) {
+        this.buyOrSell = buyOrSell;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Stock(){
+
+    }
+
 
 }
