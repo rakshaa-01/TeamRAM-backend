00726d3cb06f015ba321817ba1bf35cb0aefc2e2
Rakshaa M <divya123this@gmail.com>
2023-08-23 17:39:16 +0000

    Main class changes & test added

--- src/test/java/com/team/ram/backend/BackendApplicationTests.java
+++ src/test/java/com/team/ram/backend/BackendApplicationTests.java
@@ -1,14 +1,14 @@
 package com.team.ram.backend;
 
-import com.team.ram.backend.entities.Stock;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BackendApplicationTests {
     @Test
     public void main() {
-        BackendApplication.main(new String[] {} );
+        BackendApplication.main(new String[] {});
+        assertTrue(true);
     }
 }
