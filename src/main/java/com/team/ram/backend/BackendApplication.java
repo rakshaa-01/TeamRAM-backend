605ed7fb1504b7c75c0804303df8886dd0397db1
Rakshaa M <divya123this@gmail.com>
2023-08-23 07:48:51 +0000

    Main class changes added

--- src/main/java/com/team/ram/backend/BackendApplication.java
+++ src/main/java/com/team/ram/backend/BackendApplication.java
@@ -1,11 +1,16 @@
 package com.team.ram.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		//SpringApplication.run(BackendApplication.class, args); //Statement giving error in test main
+		new SpringApplicationBuilder(BackendApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
 	}
 }
